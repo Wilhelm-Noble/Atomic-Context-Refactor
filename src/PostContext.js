98293177daeff186fa70508672cf4c30573eb6ce
Wilhelm Noble <100698823+Wilhelm-Noble@users.noreply.github.com>
2024-07-29 15:47:01 +0800

    Add files via upload

---
+++ src/PostContext.js
@@ -0,0 +1,53 @@
+import { faker } from "@faker-js/faker";
+import { createContext, useState } from "react";
+
+const PostContext = createContext();
+
+function PostProvider({children}) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  function createRandomPost() {
+    return {
+      title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+      body: faker.hacker.phrase(),
+    };
+  }
+  return(
+  <PostContext.Provider
+    value={{
+      posts: searchedPosts,
+      onClearPosts: handleClearPosts,
+      onAddPost: handleAddPost,
+      searchQuery: searchQuery,
+      setSearchQuery: setSearchQuery,
+    }}
+  >{children}</PostContext.Provider>);
+}
+
+function usePosts() {
+    const context =  useContext(PostContext);
+    if (context === undefined) throw new Error("usePosts must be used within a PostProvider");
+    return context;
+}
+
+export  {PostProvider, usePosts};
